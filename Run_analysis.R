31532d577ed3df85d2d50336b33aca2e3b258840
EvaristeGalois93 <79868663+EvaristeGalois93@users.noreply.github.com>
2021-04-08 11:27:24 +0200

    Add files via upload
    code improved

--- Run_analysis.R
+++ Run_analysis.R
@@ -8,53 +8,64 @@
 # 5. From the data set in step 4, creates a second, independent tidy data set 
 #     with the average of each variable for each activity and each subject.
 library(dplyr)
-library(plyr)
-library(reshape)
-library(reshape2)
-library(tidyr)
+
+setwd("C:/Users/luca-/Desktop/Data_Science/Course3_GettingAndCleaningData/Working_Material")
+
 # first check folder and download data
 if(!file.exists("./data")){
   dir.create("./data")
-  }
+}
 
 fileUrl = "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 download.file(fileUrl, destfile = "./data/mydata.zip", method = "curl")
 unzip("./data/mydata.zip", exdir = "./data")
 
 # load features & labels
-features = read.table("./data/UCI HAR Dataset/features.txt")
-activ_labels = read.table("./data/UCI HAR Dataset/activity_labels.txt")
+features = read.table("./data/UCI HAR Dataset/features.txt", 
+                      col.names = c("n","functions"))
+activ_labels = read.table("./data/UCI HAR Dataset/activity_labels.txt", 
+                          col.names = c("code", "activity"))
 
 # load test data
-test_data = read.table("./data/UCI HAR Dataset/test/X_test.txt")
-test_labels = read.table("./data/UCI HAR Dataset/test/y_test.txt")
-subj_test = read.table("./data/UCI HAR Dataset/test/subject_test.txt")
+test_data = read.table("./data/UCI HAR Dataset/test/X_test.txt", 
+                       col.names = features$functions)
+test_labels = read.table("./data/UCI HAR Dataset/test/y_test.txt", 
+                         col.names = "code")
+subj_test = read.table("./data/UCI HAR Dataset/test/subject_test.txt", 
+                       col.names = "subject")
 
 # load train data
-train_data = read.table("./data/UCI HAR Dataset/train/X_train.txt")
-train_labels = read.table("./data/UCI HAR Dataset/train/y_train.txt")
-subj_train = read.table("./data/UCI HAR Dataset/train/subject_train.txt")
+train_data = read.table("./data/UCI HAR Dataset/train/X_train.txt", 
+                        col.names = features$functions)
+train_labels = read.table("./data/UCI HAR Dataset/train/y_train.txt", 
+                          col.names = "code")
+subj_train = read.table("./data/UCI HAR Dataset/train/subject_train.txt", 
+                        col.names = "subject")
 
-# merge tables
-mydata = rbind(train_data, test_data)
-mylabels = rbind(train_labels,test_labels)
-mysubj = rbind(subj_train, subj_test)
+# 1. merge tables
+my_data = rbind(train_data, test_data)
+my_label = rbind(train_labels, test_labels)
+Subject = rbind(subj_train, subj_test)
+Merged_Data = cbind(Subject, my_label, my_data)
 
-# filter features by mean and std
-features_filt = features[grepl("mean\\(|std\\(", features[,2]),]
+# 2. filter by mean and std
+Tidy_Data = Merged_Data %>% 
+  select(subject, code, contains("mean"), contains("std"))
 
-# associate labels with their respective activity
-mylabels = inner_join(mylabels, activ_labels)
+Tidy_Data$code = activ_labels[Tidy_Data$code, 2]
 
-# filter observations by features and merge tables
-mydata = mydata[,features_filt$V1]
-mydata = cbind(mysubj, mylabels, mydata)
+# 3-4. re-name columns in a descriptive manner
+names(Tidy_Data)[2] = "activity"
+names(Tidy_Data) = gsub("BodyBody", "Body", names(Tidy_Data))
+names(Tidy_Data) = gsub("^t", "Time", names(Tidy_Data))
+names(Tidy_Data) = gsub("^f", "Frequency", names(Tidy_Data))
+names(Tidy_Data) = gsub("tBody", "TimeBody", names(Tidy_Data))
+names(Tidy_Data) = gsub("-mean()", "Mean", names(Tidy_Data), ignore.case = TRUE)
+names(Tidy_Data) = gsub("-std()", "STD", names(Tidy_Data), ignore.case = TRUE)
+names(Tidy_Data) = gsub("-freq()", "Frequency", names(Tidy_Data), ignore.case = TRUE)
 
-# give descriptive names
-colnames(mydata)[1:3] =  c("Subject", "Activity", "Activity_Descr")
-
-# Point 5: finally reshape data and compute mean across activity and label
-TidyData = dcast(mydata, Subject ~ Activity, fun.aggregate = mean)
-
-# write table
-write.table(x = TidyData, file = "tidyData.txt", quote = FALSE)
+# 5. create tidy dataset
+ReshapedData = Tidy_Data %>%
+  group_by(subject, activity) %>%
+  summarise_all(funs(mean))
+write.table(x = ReshapedData, file =  "TidyData.txt", row.name = FALSE)
